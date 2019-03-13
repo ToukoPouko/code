15b7c754e1771104ad465d04afbd565c6ab432ea
TokouPouko <juhoe.vahakangas@gmail.com>
2019-03-13 18:14:12 +0200

    Desktop

--- aa.py
+++ aa.py
@@ -1,20 +1,48 @@
-import win32api, win32gui, time, numpy
+import win32gui, time, numpy, pyautogui, sys
+#from tkinter import *
 
+
+'''
+window = Tk()
+window.title("Bomba Timer")
+window.geometry("300x300")
+
+
+lblTime = Label(window, text="40.000")
+lblTime.grid(column=1, row=1)
+
+window.mainloop()
+'''
 correct = [234, 65, 65]
-defusetime = 40
+defusetime = 39.89999
 
-while True:    
-    if win32gui.GetWindowText(win32gui.GetForegroundWindow()).lower() == "counter-strike: global offensive":
-        window_id = win32gui.GetDesktopWindow()
-        window_dc = win32gui.GetWindowDC(window_id)
-        long_colour = win32gui.GetPixel(window_dc, 929, 788)
-        colour = int(long_colour)
-        full = [(colour & 0xff),  ((colour >> 8) & 0xff), ((colour >> 16) & 0xff)]
-        if numpy.array_equal(full, correct):
-            print("planted")
+
+def check():
+    global start_time
+    while True:    
+        if win32gui.GetWindowText(win32gui.GetForegroundWindow()).lower() == "counter-strike: global offensive":
+            window_id = win32gui.GetDesktopWindow()
+            window_dc = win32gui.GetWindowDC(window_id)
+            long_colour = win32gui.GetPixel(window_dc, 671, 759)
+            colour = int(long_colour)
+            full = [(colour & 0xff),  ((colour >> 8) & 0xff), ((colour >> 16) & 0xff)]
+            if numpy.array_equal(full, correct):
+                print("planted")
+                start_time = time.time()
+                break
+    timer()
+
+
+def timer():
+    while True:
+        current_time = 40 + (start_time - time.time())
+        sys.stdout.flush()
+        print(round(current_time, 5))
+        #lblTime.configure(text=str(round(current_time, 2)))
+        #window.mainloop()
+        if current_time <= 0:
             break
+    check()
 
-while True:
-    defusetime -= 0.01
-    print(defusetime)
-    time.sleep(0.01)
+if __name__ == "__main__":
+    check()
--- click_testing.py
+++ click_testing.py
@@ -1,4 +1,4 @@
-import win32api, win32gui, time
+import win32api, win32gui, time, datetime
 '''while True:
     window_id = win32gui.GetDesktopWindow()
     window_dc = win32gui.GetWindowDC(window_id)
@@ -6,6 +6,12 @@ import win32api, win32gui, time
     colour = int(long_colour)
     print((colour & 0xff),  ((colour >> 8) & 0xff), ((colour >> 16) & 0xff))'''
 
-while True:
+'''while True:
     print(win32gui.GetCursorPos())
-    time.sleep(1)   
+    time.sleep(1)   '''
+
+start_time = time.time()
+
+while True:
+    print(40 + (start_time - time.time()))
+
