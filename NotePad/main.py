84d19e8afa5774b31d8f8d4acd0a1e97e0545693
unknown <juhoe.vahakangas@gmail.com>
2019-03-21 11:14:40 +0200

    Notepad

---
+++ NotePad/main.py
@@ -0,0 +1,78 @@
+from tkinter import *
+from tkinter.font import Font
+
+class MyApp(Frame):
+    def __init__(self, master, *args, **kwargs):
+        
+        Frame.__init__(self, master, *args, **kwargs)
+        
+        self.master = master 
+
+        self.master.title("NotePad")
+
+        self.toolbar = Frame(self.master, bg="#eee")
+        self.toolbar.pack(side="top", fill="both")
+
+        '''self.btnBold = Button(self.toolbar, text="Bold", command=self.make_bold)
+        self.btnBold.pack(side="left")    
+
+        self.bold_font = Font(family="Times New Roman", size=12, weight="bold")  (bold)'''
+
+        self.normal_font = Font(family="Linux Biolinum G", size=13)
+
+        self.text = Text(self.master, font=self.normal_font, background="#212121", foreground="lightgreen", insertbackground="lightgreen")
+        self.text.focus()
+        self.text.pack(fill="both", expand=True)
+
+        #self.text.tag_configure("BOLD", font=self.bold_font)
+
+        #self.btnCheckTags = Button(self.toolbar, text="Check Tags", command=self.check_tags)
+        #self.btnCheckTags.pack(side="left")
+
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+
+        file = Menu(menu)
+        file.add_command(label="New File", command=self.new_file)
+        file.add_command(label="Open File", command=self.open_file)
+        file.add_command(label="Save File", command=self.save_file)       
+        file.add_command(label="Exit", command=self.window_exit)
+
+        menu.add_cascade(label="File", menu=file)
+
+    def new_file(self):
+        return
+
+    def open_file(self):
+        return
+
+    def save_file(self):
+        return
+
+    def make_bold(self):  
+        try:
+            if "BOLD" in self.text.tag_names():
+                print("yeet1")
+                self.text.tag_remove("BOLD", "1.0", "end")
+            elif not "BOLD" in self.text.tag_names():
+                self.text.tag_add("BOLD", "sel.first", "sel.last")
+                print("yeet2")
+        except TclError:
+            pass
+
+    def check_tags(self):
+        print(self.text.tag_names("BOLD"))
+
+    def window_exit(self):
+        exit()
+        
+
+
+
+
+root = Tk()
+root.configure
+root.geometry("500x500")
+
+app = MyApp(root)
+root.mainloop()
